bcb4a14707d13421b031c4b03bd979660a83ab9a
7savochkin <savochkinoleg17@gmail.com>
2022-09-29 12:24:25 +0300

    feat(flake8): fix flake8

--- shop/settings.py
+++ shop/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', # noqa
