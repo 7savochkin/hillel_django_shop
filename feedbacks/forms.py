cc73521eb63e5ef761927d055e3cd46ed385784b
7savochkin <savochkinoleg17@gmail.com>
2022-10-08 14:41:04 +0300

    refactor form

--- feedbacks/forms.py
+++ feedbacks/forms.py
@@ -1,7 +1,5 @@
 from django import forms
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect
 
 from feedbacks.models import Feedback
 
