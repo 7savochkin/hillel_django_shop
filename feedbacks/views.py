8a1f439f48456dac8fc4ecbdb1c01d31bd6949ab
7savochkin <savochkinoleg17@gmail.com>
2022-10-09 18:28:29 +0300

    build: create new model form

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from feedbacks.model_forms import FeedbackModelForm
 from feedbacks.models import Feedback
 
+
 @login_required
 def feedbacks(request, *args, **kwargs):
     user = request.user
@@ -13,8 +14,8 @@ def feedbacks(request, *args, **kwargs):
         form = FeedbackModelForm(user=user, data=request.POST)
         if form.is_valid():
             new_feedback = form.save(commit=False)
-            new_feedback.text = re.sub(r'[^A-Za-z0-9]+', '',
-                               f'{form.cleaned_data.get("text")}')
+            new_feedback.text = re.sub(r'(\<(/?[^>]+)>)', '',
+                                       f'{form.cleaned_data.get("text")}')
             new_feedback.save()
     else:
         form = FeedbackModelForm(user=user)
