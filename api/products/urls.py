019664096425dcaa5a79ec421417163afbee77e3
7savochkin <savochkinoleg17@gmail.com>
2022-12-16 00:40:29 +0200

    build:create api for products

--- api/products/urls.py
+++ api/products/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 
-from api.products.views import ProductsViewSet
+from api.products.views import ProductsViewSet, ProductDetailViewSet
 
 urlpatterns = [
-    path('products/', ProductsViewSet.as_view())
+    path('products/', ProductsViewSet.as_view()),
+    path('products/<uuid:pk>', ProductDetailViewSet.as_view())
 ]
