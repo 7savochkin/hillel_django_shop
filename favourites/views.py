43f87eaf3a6085970235278bc2a3eedab5d5ce84
7savochkin <savochkinoleg17@gmail.com>
2022-12-27 02:59:01 +0200

    build: ajax request on add/delete favourites

--- favourites/views.py
+++ favourites/views.py
@@ -1,11 +1,15 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.views.generic import RedirectView
+from django.utils.decorators import method_decorator
+from django.views.generic import RedirectView, DetailView
 
-from favourites.forms import AddFavouritesForm, DeleteFavouritesForm
+from favourites.forms import DeleteFavouritesForm
 from favourites.mixins import GetFavouritesMixin
 from products.forms import ProductFilterForm
+from products.models import Product
+from shop.decorators import ajax_required
 from shop.mixins.views_mixins import ProductFilterMixin
 
 
@@ -31,16 +35,43 @@ class FavouritesView(LoginRequiredMixin, GetFavouritesMixin,
         return context_data
 
 
-class AddFavouritesView(GetFavouritesMixin, RedirectView):
-    url = reverse_lazy('favourites')
+class AddOrDeleteFavouritesView(GetFavouritesMixin, DetailView):
+    model = Product
 
-    def post(self, request, *args, **kwargs):
-        form = AddFavouritesForm(request.POST,
-                                 favourites=self.get_favourites_object())
-        if form.is_valid():
-            messages.success(request, message='Product was add to favourites!')
-            form.save()
-        return self.get(request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        favourites = self.get_favourites_object()
+        product = self.get_object()
+        if product not in favourites.products.all():
+            favourites.products.add(product)
+            favourites.save()
+            messages.success(request,
+                             message='Product was added to favourites!')
+        else:
+            messages.warning(request,
+                             message='Product was deleted from favourite')
+            favourites.products.remove(product)
+            favourites.save()
+        return HttpResponseRedirect(reverse_lazy('products') + str(product.id))
+
+
+class AJAXAddOrDeleteFavouritesView(GetFavouritesMixin, DetailView):
+    model = Product
+
+    @method_decorator(ajax_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        favourites = self.get_favourites_object()
+        product = self.get_object()
+        if product not in favourites.products.all():
+            favourites.products.add(product)
+            favourites.save()
+        else:
+            favourites.products.remove(product)
+            favourites.save()
+        attached = product in favourites.products.all()
+        return JsonResponse(data={'attached': attached})
 
 
 class DeleteFavouritesView(GetFavouritesMixin, RedirectView):
